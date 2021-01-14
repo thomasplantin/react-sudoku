664979d0e0c12ff652648f82abde8e9e7d056b9a
thomasplantin <tplantin@utexas.edu>
2021-01-14 01:36:36 -0600

    grid clicking works

--- src/Cell.js
+++ src/Cell.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 
+
+function handleClick(e) {
+    console.log(e);
+}
+
 class Cell extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        alert(this.props.cell.coordinate);
+    }
+
     render() {
-        return (
-            <td className="Table-cell">
-                <input className="Cell-input" type="submit" value={this.props.cell.coordinate}></input>
-            </td>
-        );
+        if(this.props.cell.mutable) {
+            return (
+                <td className={`Table-cell ${this.props.cell.bold}`}>
+                    <input className="Cell-input" type="button" value={this.props.cell.coordinate} onClick={this.handleClick} />
+                </td>
+            );
+        } else {
+            return (
+                <td className={`Table-cell ${this.props.cell.bold}`}>
+                    <input className="Cell-input" type="button" value={this.props.cell.value} disabled />
+                </td>
+            );
+        }
     }
 }
 
--- src/Table.js
+++ src/Table.js
@@ -7,12 +7,22 @@ var rows = [];
 for(var i = 0; i < size; i++) {
     var cells = [];
     for(var j = 0; j < size; j++) {
-        let mutable = true;
+        var mutable = Math.random() > 0.5;
+        var bold;
+        if((i === 2 || i === 5) && (j === 2 || j === 5)) {
+            bold = 'Table-cell-bold-right Table-cell-bold-bottom';
+        } else if (i === 2 || i === 5) {
+            bold = 'Table-cell-bold-bottom';
+        } else if (j === 2 || j === 5) {
+            bold = 'Table-cell-bold-right';
+        } else {
+            bold = '';
+        }
         cells.push({
             coordinate: i+','+j,
             value: 'X',
             mutable: mutable,
-            bold: null
+            bold: bold
         });
     }
     rows.push(<Row key={i} cells={cells} />);
